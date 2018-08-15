efe2dabb4c74bac9f8f6ccde0dcb671faa165299
Yi-love <438002896@qq.com>
2018-08-15 20:23:14 +0800

    修复bin

--- bin/cossync.js
+++ bin/cossync.js
@@ -9,37 +9,37 @@ var conf = require(path.join(process.cwd(), process.argv[2] || 'cossyncconf.json
 var cos = cossync(conf);
 var tryTimes = 3;
 
-consloe.log('------------------------CosSync v' + version + '---------------------------');
+console.log('------------------------CosSync v' + version + '---------------------------');
 
 function callback(err , result){
-    consloe.log('[CI] <======== done.');
+    console.log('[CI] <======== done.');
     if ( err ){
-        consloe.log('[CI] is error ' + err);
+        console.log('[CI] is error ' + err);
         if ( tryTimes > 0 ){
             tryTimes --;
-            consloe.log('\n[CI] <===== will try agian. ' + tryTimes);
+            console.log('\n[CI] <===== will try agian. ' + tryTimes);
             return runCos(result);
         }
-        consloe.log('[CI] <======== has error.\n\n');
+        console.log('[CI] <======== has error.\n\n');
         process.exit(1);
     }
-    consloe.log('[CI] <======== all success.\n\n');
+    console.log('[CI] <======== all success.\n\n');
     process.exit(0);
 }
 
 function runCos(result){
-    if ( cos.version === 'v5' ){
+    if ( conf.version === 'v5' ){
         cos.upload(conf.localPath , conf.globConfig , callback);
     }else {
         cos.upload(conf.localPath, conf.mime, conf.maxAge || conf.cacheMaxAge || 0, callback);
     }
 }
 
-consloe.log('[CI] =======> will run , please wait......');
+console.log('[CI] =======> will run , please wait......');
 
-consloe.log('[CI] -------- ' + conf.localPath + ' ==> ' + conf.remotePath);
+console.log('[CI] -------- ' + conf.localPath + ' ==> ' + conf.remotePath);
 
-if ( cos.version === 'v5' ){
+if ( conf.version === 'v5' ){
     cos.sync(conf.localPath , conf.globConfig , callback);
 }else {
     cos.sync(conf.localPath, conf.mime, conf.maxAge || conf.cacheMaxAge || 0, callback);
